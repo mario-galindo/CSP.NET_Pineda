6c0c8d64891dd4e393588810a2b821734e0d4037
pineda95 <marcos_pineda95@unitec.edu>
2016-09-09 16:57:25 -0600

    primera CRUD DE Productos

---
+++ CSP.NET_Pineda/Controllers/ProductosController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSP.NET_Pineda.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private Entities db = new Entities();
+        // GET: Productos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string addProductos(string _nombreProducto,int _cantidadStock,int _categoria) {
+            try
+            {
+                Productos nuevoProducto = new Productos()
+                {
+                    NombreProducto = _nombreProducto,
+                    CantidadStock = _cantidadStock,
+                    CategoriaProducto = _categoria
+
+                };
+
+                db.Productos.Add(nuevoProducto);
+                db.SaveChanges();
+
+                return "true";
+            }
+            catch (Exception e)
+            {
+
+                return e.ToString();
+            }
+
+        }
+    }
+}
---
+++ CSP.NET_Pineda/Public/app.js
@@ -0,0 +1,47 @@
+﻿var app = angular.module("app", [
+    'ngRoute'
+]);
+
+app.config(function ($routeProvider, $locationProvider) {
+
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    }).hashPrefix('!');
+
+    $routeProvider
+
+        .when('/Productos', {
+
+            templateUrl: 'Public/Templates/Productos.html',
+            controller: 'MainController'
+        })
+        .when('/Compras', {
+
+            templateUrl: 'Public/Templates/Compras.html',
+            controller: 'MainController'
+        })
+        .otherwise({
+
+            redirectTo: '/'
+        });
+
+
+});
+
+app.controller("MainController", function ($scope, $http) {
+    $scope.Nombre = "Cafe";
+    $scope.Cantidad = "200";
+    $scope.Categoria = "1";
+
+    $scope.agregarProducto = function () {
+        $http.post('Productos/addProductos', { _nombreProducto: $scope.Nombre, _cantidadStock: $scope.Cantidad, _categoria: $scope.Categoria })
+            .success(function (data) {
+                console.log(data);
+            })
+            .error(function (error) {
+                console.log(error);
+            })
+    }
+});
+
